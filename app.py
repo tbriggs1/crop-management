f0a308de2ba338ba20d30b9e6b1b03eac460fde4
tbriggs1 <tombriggs1993@outlook.com>
2022-04-01 22:24:39 +0100

    adding dockerfiles

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
-
-from flask import Flask
-from enum import Enum, auto
+from flask import Flask, request, redirect, url_for, make_response
 
 app = Flask(__name__)
+app.secret_key = "my super secret key"
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "<h1>Homepage </h1>"
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
