abf253c446829aeb59da36b813295964cbb68ddf
tbriggs1 <tombriggs1993@outlook.com>
2022-04-01 18:05:32 +0100

    adding requirements and dockerfile

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-def main():
-    print("Hello new application")
+import os
+
+from flask import Flask
+from enum import Enum, auto
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
