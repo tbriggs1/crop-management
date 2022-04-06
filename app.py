e440fc6712aaa6af404eee7c3c35f2e281c05164
tbriggs1 <tombriggs1993@outlook.com>
2022-04-06 19:49:51 +0100

    added inital db

--- app.py
+++ app.py
@@ -1,15 +1,96 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required, current_identity
+from security import authenticate, identity
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
-from flask import Flask, request, redirect, url_for, make_response
 
 app = Flask(__name__)
-app.secret_key = "my super secret key"
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tbriggs:example@localhost/postgres'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'supersecret'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# TODO - Add library CORS(app)
+
+jwt = JWT(app, authenticate, identity)
+
+items = []
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String())
+    lastname = db.Column(db.String())
+
+    def __init__(self, id, firstname, lastname):
+        self.id = id
+        self.firstname = firstname
+        self.lastname =lastname
+
+    def __repr__(self):
+        return f"<User {self.firstname}>"
 
 
-@app.route("/")
-def index():
-    return "<h1>Homepage </h1>"
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
+    # @jwt_required()
+    def get(self, id):
+        user = UserModel.query.get_or_404(id)
+        result = {"id": id, "firstname": user.firstname, "lastname": user.lastname}
+
+        return {"user": result}
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with name '{}' already exists.".format(name)}
+
+        data = Item.parser.parse_args()
+
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item
+
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    @jwt_required()
+    def put(self, name):
+        data = Item.parser.parse_args()
+        # Once again, print something not in the args to verify everything works
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:id>')
+api.add_resource(ItemList, '/items')
 
 
 if __name__ == "__main__":
+    db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
